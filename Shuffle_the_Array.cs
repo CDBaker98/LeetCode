3dee12a5aab1489d6feeb3b371473bac466a251d
Caleb Baker <github@cdbaker98.com>
2021-06-09 15:46:27 -0500

    Create Shuffle_the_Array.cs

---
+++ Shuffle_the_Array.cs
@@ -0,0 +1,12 @@
+﻿public class Solution {
+    public int[] Shuffle(int[] nums, int n) {
+        int[] res = new int[nums.Length];
+
+        for (int i = 0; i < n; i++) {
+            res[2 * i] = nums[i];
+            res[2 * i + 1] = nums[n + i];
+        }
+
+        return res;
+    }
+}
