9e66bc471278fded4d7aa70ed493f364678b1b76
Caleb Baker <github@cdbaker98.com>
2021-06-09 15:46:29 -0500

    Create Defanging_an_IP_Address.cs

---
+++ Defanging_an_IP_Address.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public string DefangIPaddr(string address) {
+        string[] splitStrings = address.Split('.');
+
+        for (int i = 0; i < 3; i++) {
+            splitStrings[i] += "[.]";
+        }
+
+        return splitStrings[0] + splitStrings[1] + splitStrings[2] + splitStrings[3];
+    }
+}
