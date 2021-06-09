87367bbc71fc0f0daf315fa16856068293a3627c
Caleb Baker <github@cdbaker98.com>
2021-06-09 15:49:23 -0500

    Create Richest_Customer_Wealth.cs

---
+++ Richest_Customer_Wealth.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int MaximumWealth(int[][] accounts) {
+        int MaxSum = -1;
+
+        foreach (int[] i in accounts) {
+            int sum = i.Sum();
+            if (sum > MaxSum) {
+                MaxSum = sum;
+            }
+        }
+
+        return MaxSum;
+    }
+}
