33fa7cd33065bd8e5a88330990094f26f701b584
Caleb Baker <github@cdbaker98.com>
2021-06-09 15:49:27 -0500

    Create Kids_With_the_Greatest_Number_of_Candies.cs

---
+++ Kids_With_the_Greatest_Number_of_Candies.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+        IList<bool> res = new List<bool>();
+        int currMax = candies.Max();
+
+        foreach (int i in candies) {
+            if (i + extraCandies >= currMax)
+                res.Add(true);
+            else
+                res.Add(false);
+        }
+
+        return res;
+    }
+}
