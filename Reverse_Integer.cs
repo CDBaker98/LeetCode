d96cf156ffdb8eac5385ee89d249d2e71301f3bb
Caleb Baker <github@cdbaker98.com>
2021-06-09 15:36:08 -0500

    Create Reverse_Integer.cs

---
+++ Reverse_Integer.cs
@@ -0,0 +1,21 @@
+﻿public class Solution {
+    public int Reverse(int x) {
+        int res;
+        char[] xArr = x.ToString().ToCharArray();
+        if (xArr[0] == '-') {
+            for (int i = 1; i < xArr.Length; i++) {
+                xArr[i - 1] = xArr[i];
+            }
+            xArr[xArr.Length - 1] = '-';
+        }
+        Array.Reverse(xArr);
+
+        try {
+            res = int.Parse(xArr);
+            return res;
+        }
+        catch (Exception) {
+            return 0;
+        }
+    }
+}
