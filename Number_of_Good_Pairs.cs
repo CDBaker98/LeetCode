935b052922547b6fa8283ef25a3ba7f72cb7d32f
Caleb Baker <github@cdbaker98.com>
2021-06-09 15:49:29 -0500

    Create Number_of_Good_Pairs.cs

---
+++ Number_of_Good_Pairs.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int NumIdenticalPairs(int[] nums) {
+        int res = 0;
+
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] == nums[j])
+                    res++;
+            }
+        }
+
+        return res;
+    }
+}
