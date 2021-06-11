307c66b6d2a4a48a2144977859911983df9a93a9
Caleb Baker <github@cdbaker98.com>
2021-06-11 14:46:41 -0500

    Create Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.cs

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.cs
@@ -0,0 +1,25 @@
+﻿/*
+Problem 1281:
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+*/
+
+public class Solution {
+    public int SubtractProductAndSum(int n) {
+        List<int> temp = new List<int>();
+        int prod = 1, sum = 0;
+
+        temp.Insert(0, n);
+
+        for (int i = 0; temp[i] % 10 != temp[i]; i++) {
+            temp.Insert(i + 1, (temp[i] - temp[i] % 10) / 10);
+            temp[i] = temp[i] % 10;
+        }
+
+        for (int i = 0; i < temp.Count; i++) {
+            prod *= temp[i];
+            sum += temp[i];
+        }
+
+        return (prod - sum);
+    }
+}
