5dd0c39b1aec8f2d33b93e5ab330c8ca3acd92e0
Caleb Baker <github@cdbaker98.com>
2021-06-14 13:17:17 -0500

    Create Palindrome_Number.cs

---
+++ Palindrome_Number.cs
@@ -0,0 +1,24 @@
+﻿/*
+Problem 9:
+Given an integer x, return true if x is palindrome integer.
+An integer is a palindrome when it reads the same backward as forward.
+For example, 121 is palindrome while 123 is not.
+*/
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+        char[] xArr = x.ToString().ToCharArray();
+        char[] xArrRev = x.ToString().ToCharArray();
+        bool res = true;
+
+        Array.Reverse(xArrRev);
+
+        for (int i = 0; i < xArr.Length; i++) {
+            if (xArr[i] != xArrRev[i]) {
+                res = false;
+            }
+        }
+
+        return res;
+    }
+}
