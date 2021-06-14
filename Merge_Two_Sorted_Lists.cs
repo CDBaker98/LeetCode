429cc7e8ece60fe2bb70cc96b9b0d8a8d9556b10
Caleb Baker <github@cdbaker98.com>
2021-06-14 12:42:29 -0500

    Better solution

--- Merge_Two_Sorted_Lists.cs
+++ Merge_Two_Sorted_Lists.cs
@@ -13,50 +13,40 @@ public class Solution {
             return l1;
         }
 
-        List<int> temp1 = new List<int>();
-        List<int> temp2 = new List<int>();
-        List<int> resultList = new List<int>();
-        int count1 = 0, count2 = 0;
+        ListNode res;
 
-        while (l1 != null) {
-            temp1.Insert(count1, l1.val);
-            count1++;
+        if (l1.val <= l2.val) {
+            res = new ListNode(l1.val);
             l1 = l1.next;
         }
-        while (l2 != null) {
-            temp2.Insert(count2, l2.val);
-            count2++;
+        else {
+            res = new ListNode(l2.val);
             l2 = l2.next;
         }
 
-        int total = count1 + count2;
-        int ind1 = 0, ind2 = 0;
-
-        for (int i = 0; i < total; i++) {
-            if (ind1 == count1) {
-                resultList.Insert(i, temp2[ind2]);
-                ind2++;
-            }
-            else if (ind2 == count2) {
-                resultList.Insert(i, temp1[ind1]);
-                ind1++;
-            }
-            else if (temp1[ind1] <= temp2[ind2]) {
-                resultList.Insert(i, temp1[ind1]);
-                ind1++;
-            }
-            else {
-                resultList.Insert(i, temp2[ind2]);
-                ind2++;
-            }
-        }
-
-        ListNode res = new ListNode(resultList[0]);
         ListNode temp = res;
 
-        for (int i = 1; i < total; i++) {
-            temp.next = new ListNode(resultList[i]);
-            temp = temp.next;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                temp.next = new ListNode(l2.val);
+                temp = temp.next;
+                l2 = l2.next;
+            }
+            else if (l2 == null) {
+                temp.next = new ListNode(l1.val);
+                temp = temp.next;
+                l1 = l1.next;
+            }
+            else if (l1.val <= l2.val) {
+                temp.next = new ListNode(l1.val);
+                temp = temp.next;
+                l1 = l1.next;
+            }
+            else {
+                temp.next = new ListNode(l2.val);
+                temp = temp.next;
+                l2 = l2.next;
+            }
         }
 
         return res;
