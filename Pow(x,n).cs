e0a613c414101175c3bb2c55c9799b572108137b
Caleb Baker <github@cdbaker98.com>
2021-06-23 16:55:13 -0500

    Create Pow(x,n).cs

---
+++ Pow(x,n).cs
@@ -0,0 +1,10 @@
+﻿/*
+Problem 50:
+Implement pow(x, n), which calculates x raised to the power n (i.e., x^n).
+*/
+
+public class Solution {
+    public double MyPow(double x, int n) {
+        return Math.Pow(x, n); // I'm not against using the tools I have available
+    }
+}
