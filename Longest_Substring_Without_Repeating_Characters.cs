91592488fbd757cb623ef02458d66e55f69deba1
Caleb Baker <github@cdbaker98.com>
2021-06-24 12:00:42 -0500

    Create Longest_Substring_Without_Repeating_Characters.cs

---
+++ Longest_Substring_Without_Repeating_Characters.cs
@@ -0,0 +1,26 @@
+﻿/*
+Problem 3:
+Given a string s, find the length of the longest substring without repeating characters.
+*/
+
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int res = 0;
+
+        for (int i = 0; i < s.Length; i++) {
+            int temp = 0;
+            string sub = "";
+
+            for (int j = i; j < s.Length && !sub.Contains(s[j]); j++) {
+                sub += s[j];
+                temp++;
+            }
+
+            if (temp > res) {
+                res = temp;
+            }
+        }
+
+        return res;
+    }
+}
